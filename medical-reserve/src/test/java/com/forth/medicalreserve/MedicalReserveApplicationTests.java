8d88b516f90571a5a5a41f0f5384f018176ca804
黄悦轩 <3021898574@qq.com>
2025-07-07 15:43:41 +0800

    添加系统框架

---
+++ medical-reserve/src/test/java/com/forth/medicalreserve/MedicalReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forth.medicalreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalReserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
